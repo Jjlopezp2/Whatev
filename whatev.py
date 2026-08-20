745ac24492d4c758321dcb476f915a2563f186a9
Jjlopezp2 <juanjoseabcd.jj@gmail.com>
2026-08-20 07:14:07 -0500

    Update whatev.py

--- whatev.py
+++ whatev.py
@@ -4,6 +4,6 @@ from PIL import Image
 sl.title("La app de Juan José López Patinno")
 
 sl.header("I Gave up for Lent")
-st.write("I'm not even going for the bronze!")
+sl.write("I'm not even going for the bronze!")
 image = Image.open("Give_Up!.webp")
 sl.image(image, caption="I'm staying right here, man.")
