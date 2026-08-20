4701b3b6f5d46a49da2305e78bdea44405e300e6
Jjlopezp2 <juanjoseabcd.jj@gmail.com>
2026-08-20 07:19:12 -0500

    Update whatev.py

--- whatev.py
+++ whatev.py
@@ -7,3 +7,6 @@ sl.header("I Gave up for Lent")
 sl.write("I'm not even going for the bronze!")
 image = Image.open("Give_Up!.webp")
 sl.image(image, caption="I'm staying right here, man.")
+
+text = sl.text_input("What are you giving up on today?", "I'm giving up on...")
+sl.write("Nice! I would give up on ", text, " too!")
