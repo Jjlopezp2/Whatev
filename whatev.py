333fb34c36b28df2e8c9a4c0867e7319421d7199
Jjlopezp2 <juanjoseabcd.jj@gmail.com>
2026-08-20 07:13:02 -0500

    Update whatev.py

--- whatev.py
+++ whatev.py
@@ -1,3 +1,9 @@
 import streamlit as sl
+from PIL import Image
 
 sl.title("La app de Juan José López Patinno")
+
+sl.header("I Gave up for Lent")
+st.write("I'm not even going for the bronze!")
+image = Image.open("Give_Up!.webp")
+sl.image(image, caption="I'm staying right here, man.")
