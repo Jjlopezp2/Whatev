d57381725dd36af21be991e7c57a1f9130e23f8b
Jjlopezp2 <juanjoseabcd.jj@gmail.com>
2026-08-20 07:23:57 -0500

    Update whatev.py

--- whatev.py
+++ whatev.py
@@ -10,3 +10,16 @@ sl.image(image, caption="I'm staying right here, man.")
 
 text = sl.text_input("What are you giving up on today?", "I'm giving up on...")
 sl.write("Nice! I would give up on ", text, " too!")
+
+col1, col2 = sl.columns(2)
+
+with col1:
+  sl.subheader("I'm giving up on this column!")
+  sl.write("There's no point in still trying if you're not going anywhere!")
+  resp = sl.checkbox("So true, bestie.")
+  if resp:
+    sl.write("You totes get me, bro.")
+
+with col2:
+  sl.subheader("Also this one!")
+  
